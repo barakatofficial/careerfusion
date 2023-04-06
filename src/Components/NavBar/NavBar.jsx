31fa807cbfb58a0950a34ccec7726d6cd1d02874
Barakat <barak.kuchai@gmail.com>
2023-04-06 03:38:07 +0100

    Fixed The Navigation System

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
       </div>
 
       <div className="menu md:flex md:items-center z-[-1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
-        <Link to={"/home"}><li className="menuList text-[#6f6f6f] active:text-redColor mx-6 md:my-0 hover:text-blueColor" >Home</li></Link>
+        <Link to={"/"}><li className="menuList text-[#6f6f6f] active:text-redColor mx-6 md:my-0 hover:text-blueColor" >Home</li></Link>
         <Link to={"/events"}><li className="menuList text-[#6f6f6f] mx-6 md:my-0 hover:text-blueColor" >Events</li></Link>
         <li className="menuList text-[#6f6f6f] mx-6 md:my-0 hover:text-blueColor" >Resources</li>
         <li className="menuList text-[#6f6f6f] mx-6 md:my-0 hover:text-blueColor" >About</li>
